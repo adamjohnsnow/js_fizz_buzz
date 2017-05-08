ba77f30aafc2691a23edefc3e71900e21ed17f1f
Nigel Said <nigelsaid@gmail.com>
2017-05-08 16:22:11 +0100

    buzz for 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -7,4 +7,9 @@ describe("fizzBuzz", function() {
   it("should receive 4 and return 4", function() {
     expect(fizzBuzz(4)).toEqual(4);
   });
+
+  it("should receive 5 and return Buzz", function() {
+    expect(fizzBuzz(5)).toEqual('Buzz');
+  });
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,6 +1,8 @@
 function fizzBuzz(number) {
   if (number == 3) {
     return 'Fizz';
+  } else if (number == 5) {
+    return 'Buzz';
   } else {
     return number
   }
